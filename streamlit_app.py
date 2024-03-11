4225293939805d9061aa61887ab579313963f75f
Vdubourg <162983868+Vdubourg@users.noreply.github.com>
2024-03-11 13:04:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,9 @@ import streamlit as st
 name= st.text_input ("您的名字")
 st.write("哈樓"+ name)
 input="Lapin"
-list_possibilities=[("兔子"),("女人"),(男人")] 
+list_possibilities=["兔子","女人",男人"] 
+st.write("traduit lapin")
+_correct_answer="兔子"
+st.write("traduis"+input)
+for i in a range(len(list_possibilities)):
+ st.button(list_possibilities[i])
