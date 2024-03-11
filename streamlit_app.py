6578284f45c4ee51e08292a339ba5374eb6ea21e
Vdubourg <162983868+Vdubourg@users.noreply.github.com>
2024-03-11 13:04:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 name= st.text_input ("您的名字")
 st.write("哈樓"+ name)
 input="Lapin"
-list_possibilities=["兔子","女人",男人"] 
+list_possibilities=["兔子","女人","男人"] 
 st.write("traduit lapin")
 _correct_answer="兔子"
 st.write("traduis"+input)
