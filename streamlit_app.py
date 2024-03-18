32596a8a833ab0d052790e0dcb38bc1b8d90f8f6
Vdubourg <162983868+Vdubourg@users.noreply.github.com>
2024-03-18 11:42:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,5 @@ for i in range(len(list_possibilities)):
  st.button(list_possibilities[i])
 import streamlit as st 
 import pandas as pd 
-voc=pd.read_cvc('https://docs.google.com/spreadsheets/d/e/2PACX-1vRRceq5Hbua-3E4NFGHWXcPNkFWPqIxaW1DqqQWbjuOP-dLhpx035VW6L8j3y6bxBlKwPN__RvILUSP/pub?output=csv')
+voc=pd.read_cvc("https://docs.google.com/spreadsheets/d/e/2PACX-1vRRceq5Hbua-3E4NFGHWXcPNkFWPqIxaW1DqqQWbjuOP-dLhpx035VW6L8j3y6bxBlKwPN__RvILUSP/pub?output=csv")
 st.dataframe(voc)
