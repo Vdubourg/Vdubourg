3a206de9a3a13b9118e9f669c3b7fca9c3fb44e9
Vdubourg <162983868+Vdubourg@users.noreply.github.com>
2024-03-18 11:59:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st 
 import pandas as pd 
-import numphy as hp
+import numpy as hp
 voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRRceq5Hbua-3E4NFGHWXcPNkFWPqIxaW1DqqQWbjuOP-dLhpx035VW6L8j3y6bxBlKwPN__RvILUSP/pub?output=csv')
 st.dataframe(voc)
