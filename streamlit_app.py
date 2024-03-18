924bdcd65b1272c8b6def7217b648c9904f5626c
Vdubourg <162983868+Vdubourg@users.noreply.github.com>
2024-03-18 11:37:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ _correct_answer="兔子"
 st.write("traduis"+input)
 for i in range(len(list_possibilities)):
  st.button(list_possibilities[i])
+import streamlit as st 
+import pandas as pd 
+voc=pd.read_cvc('https://docs.google.com/spreadsheets/d/e/2PACX-1vRRceq5Hbua-3E4NFGHWXcPNkFWPqIxaW1DqqQWbjuOP-dLhpx035VW6L8j3y6bxBlKwPN__RvILUSP/pub?output=csv')
+st.dataframe(voc)
